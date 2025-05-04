f68963ade188a3894efd0246ad3dde3e3b3e1ef1
Hat <openaihat@gmail.com>
2025-05-04 12:49:06 +0300

    warehous (sqlalchemy)

--- app_sqlAlchemy/db.py
+++ app_sqlAlchemy/db.py
@@ -1,3 +1,322 @@
-import psycopg2
+from sqlalchemy import create_engine, ForeignKey, func, between
+from sqlalchemy.orm import DeclarativeBase, sessionmaker, Session, relationship, foreign
+from sqlalchemy import Column, Integer, String, Numeric, Date
+from sqlalchemy.sql.operators import ilike_op
+
+DB_CONFIG = {
+    "dbname": "warehouse",
+    "user": "postgres",
+    "password": "Totem151012",
+    "host": "localhost",
+    "port": "5432"
+}
 
 
+engine = create_engine(url = f"postgresql://{DB_CONFIG['user']}:{DB_CONFIG['password']}@{DB_CONFIG['host']}/{DB_CONFIG['dbname']}")
+
+
+# определяем модели таблиц
+class Base(DeclarativeBase): pass
+
+
+
+class CategoriesGoods(Base):
+    __tablename__ = "categories_goods"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    goods = relationship("Goods", back_populates="categories")
+
+
+class Customers(Base):
+    __tablename__ = "customers"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+
+    sale = relationship("OrdersSale", back_populates="customers")
+
+
+class Suppliers(Base):
+    __tablename__ = "suppliers"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+
+    order_buy = relationship("OrdersBuy", back_populates="supplier")
+
+
+class Goods(Base):
+    __tablename__ = "goods"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    quantity = Column(Integer)
+    category_id = Column(Integer, ForeignKey("categories_goods.id")) #key
+    full_cost = Column(Numeric(10, 2))
+
+
+    categories = relationship("CategoriesGoods", back_populates="goods")
+    orders_buy = relationship("OrdersBuy", back_populates="goods")
+    orders_sale = relationship("OrdersSale", back_populates="goods")
+
+
+class OrdersBuy(Base):
+    __tablename__ = "orders_buy"
+
+    id = Column(Integer, primary_key=True, index=True)
+    goods_id = Column(Integer, ForeignKey("goods.id")) #key
+    supplier_id = Column(Integer, ForeignKey("suppliers.id")) #key
+    price = Column(Numeric(10, 2))
+    quantity = Column(Integer)
+    date = Column(Date)
+
+    goods = relationship("Goods", back_populates="orders_buy")
+    supplier = relationship("Suppliers", back_populates="order_buy")
+
+
+class OrdersSale(Base):
+    __tablename__ = "orders_sale"
+
+    id = Column(Integer, primary_key=True, index = True)
+    goods_id = Column(Integer, ForeignKey("goods.id")) #key
+    customers_id = Column(Integer, ForeignKey("customers.id")) #key
+    price = Column(Numeric(10, 2))
+    quantity = Column(Integer)
+    date = Column(Date)
+
+    goods = relationship("Goods", back_populates="orders_sale")
+    customers = relationship("Customers", back_populates="sale")
+
+class BankAccount(Base):
+    __tablename__ = "bank_account"
+
+    id = Column(Integer, primary_key=True, index=True)
+    transaction_amount = Column(Numeric(15,2))
+    description = Column(String)
+    transaction_date = Column(Date)
+
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+    print("Соединение успешно")
+
+
+#====================================#
+
+
+def insert_categories(categories):
+    with Session(autoflush=False, bind=engine) as db:
+        categories_goods = CategoriesGoods(
+            name = categories
+        )
+        db.add(categories_goods)
+        db.commit()
+
+
+def insert_goods(name_goods, quantity, category_id, full_cost):
+    with Session(autoflush=False, bind=engine) as db:
+        good = Goods(name = name_goods,
+                     quantity = quantity,
+                     category_id = category_id,
+                     full_cost = full_cost)
+        db.add(good)
+        db.commit()
+
+
+def insert_bank_transaction(transaction_amount, description, transaction_date):
+    with Session(autoflush=False, bind=engine) as db:
+        transaction = BankAccount(
+            transaction_amount = transaction_amount,
+            description = description,
+            transaction_date = transaction_date
+        )
+        db.add(transaction)
+        db.commit()
+
+
+def insert_customer(customer):
+    with Session(autoflush=False, bind=engine) as db:
+        new_customer = Customers(name=customer)
+        db.add(new_customer)
+        db.commit()
+
+
+def insert_suppliers(supplier):
+    with Session(autoflush=False, bind=engine) as db:
+        new_suppliers = Suppliers(name=supplier)
+        db.add(new_suppliers)
+        db.commit()
+
+
+def insert_order_sale(goods_id, customer_id, price, quantity, date):
+    try:
+
+        with Session(autoflush=False, bind=engine) as db:
+            if not db.query(Goods).get(goods_id):
+                return ValueError("Goods not found")
+
+            if not db.query(Suppliers).get(customer_id):
+                return ValueError("Customer not found")
+
+            order_sale = OrdersSale(
+                goods_id = goods_id,
+                customers_id = customer_id,
+                price = price,
+                quantity = quantity,
+                date = date
+            )
+            db.add(order_sale)
+            db.commit()
+
+    except Exception as e:
+        return {"message": f"Error {e}"}
+
+def insert_order_buy(goods_id, supplier_id, price, quantity, date):
+    try:
+        with Session(autoflush=False, bind=engine) as db:
+            if not db.query(Goods).get(goods_id):
+                raise ValueError("Goods not found")
+
+            if not db.query(Suppliers).get(supplier_id):
+                raise ValueError("Supplier not found")
+
+            orders_buy = OrdersBuy(
+                goods_id = goods_id,
+                supplier_id = supplier_id,
+                price = price,
+                quantity = quantity,
+                date = date
+            )
+            db.add(orders_buy)
+            db.commit()
+    except Exception as e:
+        return {"message": f"Error {e}"}
+
+
+
+def get_transaction(date_1, date_2):
+    with Session(autoflush=False, bind=engine) as db:
+        transaction = db.query(
+            BankAccount.id,
+            BankAccount.transaction_amount,
+            BankAccount.description,
+            BankAccount.transaction_date
+            ).filter(between(BankAccount.transaction_date, date_1, date_2)).all()
+        return transaction
+
+
+def get_orders_sale(date1, date2):
+    with Session(autoflush=False, bind=engine) as db:
+        order_sale = db.query(
+            OrdersSale.id,
+            Goods.name.label("Товар"),
+            OrdersSale.price.label("Цена партии"),
+            OrdersSale.quantity.label("Количество в партии"),
+            OrdersSale.date.label("Дата ТН"),
+            Customers.name.label("Поставщик")
+        ).join(Goods, OrdersSale.goods_id == Goods.id
+        ).join(Customers, OrdersSale.customers_id == Customers.id
+        ).filter(between(OrdersSale.date, date1, date2)).all()
+
+        return order_sale
+
+
+def get_orders_buy(date_1, date_2):
+    with Session(autoflush=False, bind=engine) as db:
+        orders_buy = db.query(
+            OrdersBuy.id,
+            Goods.name.label("Товар"),
+            OrdersBuy.price.label("Цена партии"),
+            OrdersBuy.quantity.label("Количество в партии"),
+            OrdersBuy.date.label("Дата ТН"),
+            Suppliers.name.label("Поставщик")
+        ).join(Goods, OrdersBuy.goods_id == Goods.id
+        ).join(Suppliers, OrdersBuy.supplier_id == Suppliers.id
+        ).filter(between(OrdersBuy.date, date_1, date_2)).all()
+
+        return orders_buy
+
+
+def get_goods():
+    with Session(autoflush=False, bind=engine) as db:
+        goods = db.query(
+            Goods.id,
+            Goods.name.label("Товар"),
+            Goods.quantity.label("Количество"),
+            Goods.full_cost.label("Полная стоимость"),
+            CategoriesGoods.name.label("Категория")
+            ).join(CategoriesGoods, Goods.category_id == CategoriesGoods.id, isouter = True).all()
+        return goods
+
+
+def get_goods_by_categories():
+    with Session(autoflush=False, bind=engine) as db:
+        goods_by_categories = db.query(
+            CategoriesGoods.name.label("Категория"),
+            func.sum(Goods.quantity).label("Количество"),
+            func.sum(Goods.full_cost).label("Общая стоимость")
+            ).join(Goods, CategoriesGoods.id == Goods.category_id, isouter = True).group_by(CategoriesGoods.name).all()
+        return goods_by_categories
+
+
+def get_orders_buy_suppliers(supplier):
+    with Session(autoflush=False, bind=engine) as db:
+        orders_buy = db.query(
+            OrdersBuy.id,
+            Goods.name.label("Товар"),
+            OrdersBuy.price.label("Цена партии"),
+            OrdersBuy.quantity.label("Количество в партии"),
+            OrdersBuy.date.label("Дата ТН"),
+            Suppliers.name.label("Поставщик")
+        ).join(Goods, OrdersBuy.goods_id == Goods.id
+        ).join(Suppliers, OrdersBuy.supplier_id == Suppliers.id
+        ).filter(Suppliers.name.ilike(f"%{supplier}%")).all()
+
+        return orders_buy
+
+
+def insert_orders_sale(goods_id, customers_id, price, quantity, date):
+    with Session(autoflush=False, bind=engine) as db:
+        order_sale = OrdersSale(
+            goods_id = goods_id,
+            customers_id = customers_id,
+            price = price,
+            quantity = quantity,
+            date = date
+        )
+        db.add(order_sale)
+        db.commit()
+
+
+def get_orders_sale_by_client(client):
+    with Session(autoflush=False, bind=engine) as db:
+        orders_sale = db.query(
+            OrdersSale.id,
+            Goods.name.label("Товар"),
+            OrdersSale.price.label("Цена партии"),
+            OrdersSale.quantity.label("Количество в партии"),
+            OrdersSale.date.label("Дата ТН"),
+            Customers.name.label("Поставщик")
+        ).join(Goods, OrdersSale.goods_id == Goods.id
+        ).join(Customers, OrdersSale.customers_id == Customers.id
+        ).filter(Customers.name.ilike(f"%{client}%")).all()
+
+        return orders_sale
+
+
+
+def get_orders_buy_suppliers(supplier):
+    with Session(autoflush=False, bind=engine) as db:
+        orders_buy = db.query(
+            OrdersBuy.id,
+            Goods.name.label("Товар"),
+            OrdersBuy.price.label("Цена партии"),
+            OrdersBuy.quantity.label("Количество в партии"),
+            OrdersBuy.date.label("Дата ТН"),
+            Suppliers.name.label("Поставщик")
+        ).join(Goods, OrdersBuy.goods_id == Goods.id
+        ).join(Suppliers, OrdersBuy.supplier_id == Suppliers.id
+        ).filter(Suppliers.name.ilike(f"%{supplier}%")).all()
+
+        return orders_buy
---
+++ app_sqlAlchemy/main.py
@@ -0,0 +1,310 @@
+from http import HTTPStatus
+from flask import Flask, jsonify, request
+import db
+
+app = Flask(__name__)
+
+
+# проверка подключения
+@app.route("/", methods=["GET"])
+def ping():
+    return jsonify({"message": "Server is start"}), HTTPStatus.OK
+
+
+######
+#POST#
+######
+
+
+# добавление товара
+@app.route("/insert_goods", methods=["POST"])
+def insert_goods():
+    good = request.get_json()
+
+    if good['name_goods'] is None or good['quantity'] is None or good['category_id'] is None or good['full_cost'] is None:
+        return jsonify({"error": "info incorrect"}), HTTPStatus.BAD_REQUEST
+    elif good['name_goods'] == '' or good['quantity'] == '' or good['category_id'] == '' or good[
+        'full_cost'] == '':
+        return jsonify({"error": "info incorrect"}), HTTPStatus.BAD_REQUEST
+
+    db.insert_goods(good['name_goods'], good['quantity'], good['category_id'], good['full_cost'])
+
+    return jsonify({"message": "good added"}), HTTPStatus.CREATED
+
+
+# добавление операции покупки товара
+@app.route("/insert_order_buy", methods=["POST"])
+def insert_order_buy():
+    buy_info = request.get_json()
+
+    if buy_info['goods_id'] is None or buy_info['supplier_id'] is None or buy_info['price'] is None or buy_info[
+        'quantity'] is None or buy_info['date'] is None:
+
+        return jsonify({"error": "info incorrect"}), HTTPStatus.BAD_REQUEST
+    elif buy_info['goods_id'] == '' or buy_info['supplier_id'] == '' or buy_info['price'] == '' or buy_info[
+        'quantity'] == '' or buy_info['date'] == '':
+
+        return jsonify({"error": "info incorrect"}), HTTPStatus.BAD_REQUEST
+
+    db.insert_order_buy(buy_info['goods_id'], buy_info['supplier_id'], buy_info['price'], buy_info[
+        'quantity'], buy_info['date'])
+
+    return jsonify({"message": "buy info added in BD"}), HTTPStatus.CREATED
+
+
+# добавление операции реализации товара
+@app.route("/insert_order_sale", methods=["POST"])
+def insert_order_sale():
+    sale_info = request.get_json()
+
+    if sale_info['goods_id'] is None or sale_info['customer_id'] is None or sale_info['price'] is None or sale_info[
+        'quantity'] is None or sale_info['date'] is None:
+
+        return jsonify({"error": "info incorrect"}), HTTPStatus.BAD_REQUEST
+    elif sale_info['goods_id'] == '' or sale_info['customer_id'] == '' or sale_info['price'] == '' or sale_info[
+        'quantity'] == '' or sale_info['date'] == '':
+
+        return jsonify({"error": "info incorrect"}), HTTPStatus.BAD_REQUEST
+
+    db.insert_order_sale(sale_info['goods_id'], sale_info['customer_id'], sale_info['price'], sale_info[
+        'quantity'], sale_info['date'])
+
+    return jsonify({"message": "sale info added in BD"}), HTTPStatus.CREATED
+
+
+# добавление банковской операции
+@app.route("/insert_bank_transaction", methods=["POST"])
+def insert_bank_transaction():
+    transaction = request.get_json()
+
+    if transaction['transaction_amount'] is None or transaction['description'] is None or transaction['transaction_date'] is None:
+        return jsonify({"error": "info incorrect"}), HTTPStatus.BAD_REQUEST
+    elif transaction['transaction_amount'] == '' or transaction['description'] == '' or transaction['transaction_date'] == '':
+        return jsonify({"error": "info incorrect"}), HTTPStatus.BAD_REQUEST
+
+    db.insert_bank_transaction(transaction['transaction_amount'], transaction['description'], transaction['transaction_date'])
+
+    return jsonify({"message": "added bank transaction"}), HTTPStatus.CREATED
+
+
+# добавление категории товара
+@app.route("/insert_categories_goods", methods=["POST"])
+def insert_categories_goods():
+    categories = request.get_json()
+    if "name" not in categories:
+        return jsonify({"error": "json incorrect, not key 'name"}), HTTPStatus.BAD_REQUEST
+    if categories['name'] is None:
+        return jsonify({"error": "info incorrect"}), HTTPStatus.BAD_REQUEST
+    elif categories['name'] == '':
+        return jsonify({"error": "info incorrect"}), HTTPStatus.BAD_REQUEST
+
+    db.insert_categories(categories['name'])
+
+    return jsonify({"message": "added categories in BD"}), HTTPStatus.CREATED
+
+
+# добавление поставщика
+@app.route("/insert_suppliers", methods=["POST"])
+def insert_suppliers():
+    suppliers = request.get_json()
+    if "name" not in suppliers:
+        return jsonify({"error": "json incorrect, not key 'name"}), HTTPStatus.BAD_REQUEST
+    if suppliers['name'] is None:
+        return jsonify({"error": "info incorrect"}), HTTPStatus.BAD_REQUEST
+    elif suppliers['name'] == '':
+        return jsonify({"error": "info incorrect"}), HTTPStatus.BAD_REQUEST
+
+    db.insert_suppliers(suppliers['name'])
+
+    return jsonify({"message": "added suppliers in BD"}), HTTPStatus.CREATED
+
+
+# добавление клиента
+@app.route("/insert_customers", methods=["POST"])
+def insert_customers():
+    customers = request.get_json()
+    if "name" not in customers:
+        return jsonify({"error": "json incorrect, not key 'name"}), HTTPStatus.BAD_REQUEST
+    if customers['name'] is None:
+        return jsonify({"error": "info incorrect"}), HTTPStatus.BAD_REQUEST
+    elif customers['name'] == '':
+        return jsonify({"error": "info incorrect"}), HTTPStatus.BAD_REQUEST
+
+    db.insert_customer(customers['name'])
+
+    return jsonify({"message": "added customers in BD"}), HTTPStatus.CREATED
+
+
+#####
+#GET#
+#####
+
+
+# получение банковский транзакций
+@app.route("/transactions/<string:date1>/<string:date2>", methods=["GET"])
+def get_transactions_by_period(date1, date2):
+
+    # нужна лучшая валидация дат!!
+    if date1 is not None and date2 is not None:
+        try:
+            transactions_info = db.get_transaction(date1, date2)
+            transactions = list()
+
+            for tran in transactions_info:
+                transactions.append({
+                    "id": tran[0],
+                    "transaction_amount": tran[1],
+                    "description": tran[2],
+                    "transaction_date": tran[3]
+                })
+            return jsonify({"transactions": transactions}), HTTPStatus.OK
+        except Exception as e:
+            return jsonify({"message": f"Error - {e}"}), HTTPStatus.BAD_REQUEST
+    else:
+        return jsonify({"message": "incorrect date in API"}), HTTPStatus.BAD_REQUEST
+
+
+# движение релаизации товара
+@app.route("/order_sale/<string:date1>/<string:date2>", methods=["GET"])
+def get_orders_sale(date1, date2):
+
+    # нужна лучшая валидация дат!!
+    if date1 is not None and date2 is not None:
+        try:
+            orders_sale = db.get_orders_sale(date1, date2)
+            sale = list()
+
+            for row in orders_sale:
+                sale.append({
+                    "id": row[0],
+                    "good_name": row[1],
+                    "price": row[2],
+                    "quantity": row[3],
+                    "date": row[4],
+                    "customers_name": row[5]
+                })
+            return jsonify({"orders_sale": sale}), HTTPStatus.OK
+        except Exception as e:
+            return jsonify({"message": f"Error - {e}"}), HTTPStatus.BAD_REQUEST
+    else:
+        return jsonify({"message": "incorrect date in API"}), HTTPStatus.BAD_REQUEST
+
+
+# движение закупки товара
+@app.route("/order_buy/<string:date1>/<string:date2>", methods=["GET"])
+def get_orders_buy(date1, date2):
+
+    # нужна лучшая валидация дат!!
+    if date1 is not None and date2 is not None:
+        try:
+            orders_buy = db.get_orders_buy(date1, date2)
+            buy = list()
+
+            for row in orders_buy:
+                buy.append({
+                    "id": row[0],
+                    "good_name": row[1],
+                    "price": row[2],
+                    "quantity": row[3],
+                    "date": row[4],
+                    "supplier_name": row[5]
+                })
+            return jsonify({"orders_buy": buy}), HTTPStatus.OK
+        except Exception as e:
+            return jsonify({"message": f"Error - {e}"}), HTTPStatus.BAD_REQUEST
+    else:
+        return jsonify({"message": "incorrect date in API"}), HTTPStatus.BAD_REQUEST
+
+
+# инфо по товарам
+@app.route("/goods_info", methods=["GET"])
+def get_goods():
+    goods_info = db.get_goods()
+
+    goods = list()
+
+    for good in goods_info:
+        goods.append({
+            "id": good[0],
+            "name": good[1],
+            "quantity": good[2],
+            "full_cost": good[3],
+            "category_name": good[4]
+        })
+
+    return jsonify({"goods": goods}), HTTPStatus.OK
+
+
+# инфо по товарам в разрезе категорий
+@app.route("/category_goods_info", methods=["GET"])
+def get_goods_category():
+    goods_info = db.get_goods_by_categories()
+
+    goods = list()
+
+    for good in goods_info:
+        goods.append({
+            "category": good[0],
+            "sum_quantity": good[1],
+            "sum_full_cost": good[2]
+        })
+
+    return jsonify({"goods_by_category": goods}), HTTPStatus.OK
+
+
+# поиск движения товара по лайк клиента
+@app.route("/search_order_by_client/<string:client>", methods=["GET"])
+def get_orders_sale_by_client(client):
+    if client is not None and client != "":
+        orders_by_client = db.get_orders_sale_by_client(client)
+
+        obc = list()
+
+        for row in orders_by_client:
+            obc.append({
+                "id": row[0],
+                "good_name": row[1],
+                "price": row[2],
+                "quantity": row[3],
+                "date": row[4],
+                "client_name": row[5]
+            })
+
+        return jsonify({"orders_by_client": obc}), HTTPStatus.OK
+    return jsonify({"message": "error"}), HTTPStatus.BAD_REQUEST
+
+
+# поиск движения товара по лайк поставщика
+@app.route("/search_order_by_suppliers/<string:suppliers>", methods=["GET"])
+def get_orders_buy_suppliers(suppliers):
+    if suppliers is not None and suppliers != "":
+        orders_by_suppliers = db.get_orders_buy_suppliers(suppliers)
+
+        obs = list()
+
+        for row in orders_by_suppliers:
+            obs.append({
+                "id": row[0],
+                "good_name": row[1],
+                "price": row[2],
+                "quantity": row[3],
+                "date": row[4],
+                "supplier_name": row[5]
+            })
+
+        return jsonify({"orders_by_suppliers": obs}), HTTPStatus.OK
+    return jsonify({"message": "error"}), HTTPStatus.BAD_REQUEST
+
+#=================#
+#Запуск приложения#
+#=================#
+
+
+if __name__ == "__main__":
+    try:
+        db.init_db()
+        print("BD init!")
+    except Exception as e:
+        print(f"Error init DB - {e}")
+
+    app.run(port=5005, debug=True)
